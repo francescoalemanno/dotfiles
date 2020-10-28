ae71b9f7ca2f8147b4b77a284f908e8b7a506b84
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-10-28 09:59:55 +0100

    added shortcuts to change windows under leader key, prompting when new buffer are created (taken form TECOSAUR)

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -340,3 +340,27 @@
         :localleader
         :desc "Biblio lookup" "l" #'biblio-lookup)
   )
+
+
+
+
+(setq evil-vsplit-window-right t
+      evil-split-window-below t)
+
+(defadvice! prompt-for-buffer (&rest _)
+  :after '(evil-window-split evil-window-vsplit)
+  (+ivy/switch-buffer))
+
+(setq +ivy-buffer-preview t)
+(map! :map evil-window-map
+     ;; "SPC" #'rotate-layout
+      ;; Navigation
+      "<left>"     #'evil-window-left
+      "<down>"     #'evil-window-down
+      "<up>"       #'evil-window-up
+      "<right>"    #'evil-window-right
+      ;; Swapping windows
+      "C-<left>"       #'+evil/window-move-left
+      "C-<down>"       #'+evil/window-move-down
+      "C-<up>"         #'+evil/window-move-up
+      "C-<right>"      #'+evil/window-move-right)
